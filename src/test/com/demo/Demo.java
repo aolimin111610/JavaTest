220187bfa44a832ad5dec6adfbbb661867379964
ao <2803806216@qq.com>
2019-05-09 20:56:21 +0800

    Initial commit

---
+++ src/test/com/demo/Demo.java
@@ -0,0 +1,11 @@
+package test.com.demo;
+
+import java.util.Date;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(new Date());
+    }
+}
